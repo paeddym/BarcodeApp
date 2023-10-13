6276089180c9be63c32f8fa0587068964f194a7c
paeddym <patrikmarosvary@gmail.com>
2023-10-13 16:47:24 +0200

    Expanded Data Output

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,29 @@ export default {
   },
   data(){
     return {
-      code: 0
+      code: undefined,
+      name: undefined,
+      cost: undefined
     }
   },
   methods: {
     onDecode (result) { 
-      console.log(result) 
+
       this.code = result
+
+      if(result == 4337185051937){          //Statische QR Codes, um Datenbank/API zu umgehen. 
+          this.name = "K-Classic Wasser"    //if statt switch, da switch nicht funktioniert hat
+          this.cost = 0.29
+      } else if(result == 90446832){
+        this.name = "Red Bull"
+        this.cost = 1.49
+      } else if(result == 4009228120077){
+        this.name = "Krumbach Wasser"
+        this.cost= 0.89
+      } else {
+        this.name = "unbekannt"
+        this.cost = "?"
+      }
     },
   },
 };
@@ -26,5 +42,7 @@ export default {
   <body>
       <StreamBarcodeReader id="VideoStream" @decode="onDecode"></StreamBarcodeReader>
       <p>The Barcode is: {{ code }}</p>
+      <p>Your product is: {{ name }}</p>
+      <p>The product costs {{ cost }}€</p>
   </body>
 </template>
