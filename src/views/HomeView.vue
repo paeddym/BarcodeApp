e323371c432801df2232a9f79b9f8a495ee46e35
paeddym <patrikmarosvary@gmail.com>
2023-10-13 15:15:01 +0200

    Added VideoStream

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+  mounted() {
+    this.startCamera();
+  },
+  methods: {
+    onDecode (result) { 
+      console.log(result) 
+    },
+    async startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.$refs.videoElement.srcObject = stream;
+      } catch (error) {
+        console.error('Fehler beim Zugriff auf die Kamera:', error);
+      }
+    },
+  },
+};
 </script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <video ref="videoElement" autoplay id="VideoStream"></video>
+    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
   </main>
 </template>
+
+<style>
+#VideoStream {
+  transform: scaleX(-1);
+}
+</style>
